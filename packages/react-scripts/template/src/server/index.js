0b2bdb93fc808868db4f199d6b6eb72e770c263d
denisgoryaynov <denis.gor27@gmail.com>
2018-11-03 17:54:50 +0100

    Updated server template

--- packages/react-scripts/template/src/server/index.js
+++ packages/react-scripts/template/src/server/index.js
@@ -1,3 +1,31 @@
+const path = require('path');
+
+//node_modules dependencies
 const express = require('express');
 
+//custom dependencies
+
 const app = express();
+
+const port = process.argv[2] || 3000;
+
+app.all('*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'HEAD');
+  next();
+});
+
+app.head('/', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.use(
+  'static',
+  express.static(path.join(__dirname, 'static'), { maxAge: 604800 * 1000 })
+);
+
+app.get('*', (req, res) => {});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
